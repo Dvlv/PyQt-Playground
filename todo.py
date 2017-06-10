2d141f754bf9a917e357c64d45e0e43ea086eb25
Dvlv <dvd292@googlemail.com>
2017-06-10 15:25:50 +0100

    scrolling resizing canvas now done with wrapping task text

--- todo.py
+++ todo.py
@@ -2,25 +2,70 @@ import sys
 import PyQt5.QtWidgets as qtw
 import PyQt5.QtGui as qtg
 import PyQt5.QtCore as qtc
-from PyQt5.QtCore import Qt as qt
 
 
-class canvasWidget(qtw.QWidget):
-    def __init__(self, layout):
+class TaskDisplayWidget(qtw.QWidget):
+    def __init__(self):
         super().__init__()
-        self.setLayout(layout)
+
+        self.colour_schemes = {
+            0: "background: lightgrey; color: black;",
+            1: "background: grey; color: white;;"
+        }
+
+        default_task = qtw.QLabel(self)
+        default_task.setText("--- Add Tasks Here ---")
+        default_task.setStyleSheet(self.colour_schemes[0])
+        default_task.setAlignment(qtc.Qt.AlignCenter)
+        default_task.setFixedHeight(75)
+
+        vbox = qtw.QVBoxLayout()
+        vbox.setAlignment(qtc.Qt.AlignTop)
+        vbox.addWidget(default_task, 1)
+        vbox.setContentsMargins(0,0,0,0)
+        vbox.setSizeConstraint(qtw.QLayout.SetMinAndMaxSize)
+
+        self.layout = vbox
+
+        self.setLayout(self.layout)
+
+        self.tasks = [default_task]
+        self.maxWidth = 200
+
+    def addTask(self, task_text):
+        task = qtw.QLabel(self)
+        task.setWordWrap(1)
+        task.setGeometry(0,0,200,100)
+        task.setText(task_text)
+        task.setAlignment(qtc.Qt.AlignCenter)
+        task.setFixedHeight(75)
+
+        _, colour_scheme_choice = divmod(len(self.tasks), 2)
+        task.setStyleSheet(self.colour_schemes[colour_scheme_choice])
+        task.setMaximumWidth(self.maxWidth)
+
+        number_of_widgets = self.layout.count()
+        self.layout.insertWidget(number_of_widgets+1, task)
+        self.tasks.append(task)
+
+    def setTaskMaxWidths(self, width):
+        self.maxWidth = width
+        for index in range(self.layout.count()):
+            self.layout.itemAt(index).widget().setMaximumWidth(self.maxWidth)
+
+
+class TaskCreateTextEdit(qtw.QTextEdit):
+    def keyPressEvent(self, event):
+        if event.key() == qtc.Qt.Key_Return:
+            self.parent().addTask()
+        else:
+            return super().keyPressEvent(event)
+
 
 class App(qtw.QWidget):
     def __init__(self):
         super().__init__()
         self.title = 'Todo List'
-
-        self.colour_schemes = {
-            0: "background: lightgrey; color: black; padding:20px;",
-            1: "background: grey; color: white; padding:20px;"
-        }
-
-        self.tasks = []
 
         self.initUI()
 
@@ -30,20 +75,9 @@ class App(qtw.QWidget):
 
         #widgets
 
-        # tasks
-        default_task = qtw.QLabel(self)
-        default_task.setText("--- Add Tasks Here ---")
-        default_task.setStyleSheet(self.colour_schemes[0])
-        #default_task.setContentsMargins(0, 0, 0, 0)
-        default_task.setAlignment(qt.AlignCenter)
-        default_task.setFixedHeight(100)
-
-        self.default_task = default_task
-        self.tasks.append(self.default_task)
-
         # text input
-        text_input = qtw.QTextEdit(self)
-        text_input.setAlignment(qt.AlignBottom)
+        text_input = TaskCreateTextEdit(self)
+        text_input.setAlignment(qtc.Qt.AlignBottom)
         text_input.setMaximumHeight(75)
 
         #button
@@ -53,22 +87,13 @@ class App(qtw.QWidget):
         self.text_input = text_input
 
         # vertical layout
-        vbox = qtw.QVBoxLayout()
-        vbox.setAlignment(qt.AlignTop)
-        vbox.addWidget(self.default_task, 1)
-        vbox.setContentsMargins(0,0,0,0)
-        vbox.setSizeConstraint(qtw.QLayout.SetMinAndMaxSize)
-        self.vbox = vbox
-
-        self.cw = canvasWidget(vbox)
-        #self.resize(cw.sizeHint())
+        self.task_display = TaskDisplayWidget()
 
         # scrolling
         self.scrollArea = qtw.QScrollArea(self)
+        self.scrollArea.setVerticalScrollBarPolicy(qtc.Qt.ScrollBarAlwaysOn)
         self.scrollArea.widgetResizable()
-        #self.scrollArea.setMaximumHeight(300)
-        self.scrollArea.setWidget(self.cw)
-        #self.scrollArea.setLayout(tl)
+        self.scrollArea.setWidget(self.task_display)
         self.scrollArea.setContentsMargins(0, 0, 0, 0)
 
         self.mainLayout = qtw.QVBoxLayout(self)
@@ -84,19 +109,9 @@ class App(qtw.QWidget):
 
     def addTask(self):
         task_text = self.text_input.toPlainText()
-        task = qtw.QLabel(self)
-        task.setText(task_text)
-        task.setAlignment(qt.AlignCenter)
-        task.setFixedHeight(100)
-
-        # choose colour scheme
-        _, colour_scheme_choice = divmod(len(self.tasks), 2)
-        task.setStyleSheet(self.colour_schemes[colour_scheme_choice])
-        self.tasks.append(task)
-
-        # calculate position to insert at
-        number_of_widgets = self.vbox.count()
-        self.vbox.insertWidget(number_of_widgets+1, task)
+        if task_text:
+            self.task_display.addTask(task_text)
+            self.text_input.clear()
 
 
     def center(self):
@@ -106,7 +121,8 @@ class App(qtw.QWidget):
         self.move(frameGeometry.topLeft())
 
     def resizeEvent(self, event):
-        self.cw.resize(event.size().width() - 25, 200)
+        self.task_display.resize(event.size().width() - 25, 200)
+        self.task_display.setTaskMaxWidths(event.size().width() - 25)
 
 
 if __name__ == '__main__':
