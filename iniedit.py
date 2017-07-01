f830c077ddf9081d2f0ed366a47b656f75b2acb9
Dvlv <dvd292@googlemail.com>
2017-07-01 10:42:37 +0100

    right panel

--- iniedit.py
+++ iniedit.py
@@ -27,6 +27,9 @@ class FileHandler():
 
         return self.parser.sections()
 
+    def getParser(self):
+        return self.parser
+
 
 class MainWindow(qtw.QMainWindow):
     def __init__(self):
@@ -76,6 +79,9 @@ class MainWindow(qtw.QMainWindow):
             fileName = ": ".join([ntpath.basename(fileToOpen), fileToOpen])
             self.setWindowTitle(fileName)
 
+    def getParser(self):
+        return self.fileHandler.getParser()
+
     def fileSave(self):
         pass
 
@@ -119,6 +125,7 @@ class App(qtw.QWidget):
         self.mainLayout.setAlignment(qtc.Qt.AlignLeft)
 
         self.mainLayout.addLayout(self.leftBox)
+        self.mainLayout.addStretch(1)
         self.mainLayout.addLayout(self.rightBox)
         self.mainLayout.addStretch(1)
 
@@ -129,13 +136,58 @@ class App(qtw.QWidget):
             return
 
         for section in sections:
-            #listwidgetitem = qtw.QListWidgetItem(section)
             self.sections.addItem(section)
 
         self.sections.itemClicked.connect(self.sectionClicked)
 
     def sectionClicked(self, chosenSection):
-        print(chosenSection.text())
+        self.populateRightBox(chosenSection.text())
+
+    def clearRightBox(self):
+        for i in reversed(range(self.rightBox.count())):
+            try:
+                self.rightBox.takeAt(i).widget().setParent(None)
+            except AttributeError:
+                try:
+                    item = self.rightBox.takeAt(i)
+                    if item is not None:
+                        item.widget().deleteLater()
+                except Exception as e:
+                    print(str(e))
+
+    def populateRightBox(self, section):
+        self.clearRightBox()
+
+        parser = self.parent().getParser()
+        for key in sorted(parser[section]):
+            sectionLabel = qtw.QLabel()
+            sectionLabel.setText(key)
+            self.rightBox.addWidget(sectionLabel)
+
+            value = parser[section][key]
+            if value.isnumeric():
+                input = qtw.QSpinBox()
+                input.setValue(int(value))
+            else:
+                input = qtw.QLineEdit()
+                input.setText(value)
+            self.rightBox.addWidget(input)
+
+        self.rightBox.addStretch(1)
+        self.rightBox.addSpacing(30)
+
+        saveButton = qtw.QPushButton("Save")
+        saveButton.clicked.connect(self.saveSection)
+        self.rightBox.addWidget(saveButton)
+
+        addSectionButton = qtw.QPushButton("Add New Section")
+        addSectionButton.clicked.connect(self.addSection)
+        self.rightBox.addWidget(addSectionButton)
+
+    def saveSection(self):
+        pass
+    def addSection(self):
+        pass
 
 
 if __name__ == '__main__':
